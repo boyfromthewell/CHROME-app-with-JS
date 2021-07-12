3a5f1aa029e489fbaa04af01238035f996c8e233
boyfromthewell <tnsdyd10@naver.com>
2021-07-12 17:23:49 +0900

    JS first studying

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log(12345);
+const a=5; //
+const b=2; // const = 값 변경 불가, let = 값 변경 가능
+let myName="soonyong"; //string
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " +myName);
+
+myName="kwon";
+console.log("hello " +myName);
+
+const c= true;
+
+const dayOfWeek=["mon", "tue", "wed", "thu", "fri", "sat"]; //array
+
+console.log(dayOfWeek[5]); //index 불러오기
+
+dayOfWeek.push("sun"); // 값 추가
+
+console.log(dayOfWeek);
