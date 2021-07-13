f1364371a28671888d8327bbf77ea408f258e9eb
boyfromthewell <tnsdyd10@naver.com>
2021-07-14 00:31:20 +0900

    study JS

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const a=5; //
+const a=5; // var->쓰지말기(오래됨)
 const b=2; // const = 값 변경 불가, let = 값 변경 가능
 let myName="soonyong"; //string
 
@@ -19,3 +19,59 @@ console.log(dayOfWeek[5]); //index 불러오기
 dayOfWeek.push("sun"); // 값 추가
 
 console.log(dayOfWeek);
+
+const player ={ //object 생성
+    name:"sooon",
+    points:10,
+    fat:false,
+};
+console.log(player);
+console.log(player.name);
+
+player.fat=true;
+player.lastName="kwon";
+console.log(player);
+
+function sayHello(nameOfPerson, age){ //함수 활용
+    console.log("hello my name is "+ nameOfPerson+" and i'm " +age);
+}
+
+sayHello("soon", 12);
+sayHello("kwon", 20);
+
+function plus(first, second){
+    console.log(a+b);
+}
+
+plus(8,10);
+
+const playerr={
+    name: "nico",
+    sayHello: function(otherPersonName)
+    {
+        console.log("hello" + otherPersonName+" nice to meet you");
+    }
+};
+
+console.log(playerr.name);
+playerr.sayHello("yoon");
+
+const calculator={
+    add: function(a,b){
+        console.log(a+b);
+    },
+    minus: function(a,b){
+        console.log(a-b);
+    },
+    div: function(a,b){
+        console.log(a/b);
+    },
+    power: function(a,b){
+        console.log(a**b);
+    }
+};
+
+calculator.add(1,2);
+calculator.minus(3,2);
+calculator.div(5,2);
+calculator.power(2,5);
