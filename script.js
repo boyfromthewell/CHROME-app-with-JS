5d24a58abe39c07e7654b8298dcaf88c9429464b
boyfromthewell <tnsdyd10@naver.com>
2021-07-18 15:58:16 +0900

    JS studying

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const loginForm=document.querySelector("#login-form");
 const loginInput=document.querySelector("#login-form input");
+const greeting=document.querySelector("#greeting");
 
-const link=document.querySelector("a");
-function handleLogin(event){
+const HIDDEN_CLASSNAME="hidden";
 
-   event.preventDefault(); //기본 동작 막음(서브밋 이벤트 멈춤 진행 x)
-   console.log(loginInput.value);
+function loginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username=loginInput.value;
+    greeting.innerText="Hello "+username;
+    // greeting.innerText=`Hello ${username}`; (윗줄과 동일함, 변수 다루기)
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    
 }
-function handleLink(event){
-    event.preventDefault(); //기본 동작 막음
-}
 
-loginForm.addEventListener("submit", handleLogin);
-link.addEventListener("click", handleLink);
+loginForm.addEventListener("submit", loginSubmit);
